3e01985e278c09dbcbcd0e1f1553211b8ec66d79
Urja Saha <us64@cornell.edu>
2026-07-01 23:06:30 -0400

    made translation api call and set up /translate endpt

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const connection = require("./db")
 const express = require("express")
+const {translate} = require("@vitalets/google-translate-api")
 
 const app = express()
+
+app.use(express.json())
 
 const PORT = 8081
 
@@ -22,6 +25,18 @@ app.get("/login_info", (req, res) => {
             res.json(response)
         }
     })
+})
+
+//try using translation api
+app.post("/translate", async (req, res) => {
+    const {text, target_lang} = req.body
+    try{
+      const result = await translate(text, {to: target_lang})  
+        res.json(result)
+    }
+    catch (err){
+        console.log(err)
+    }
 })
 
 app.listen(PORT, () => {
